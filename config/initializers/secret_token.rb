d951cc3e2677909ba759b5cf3ee6c2345349e498
Matt Smith <matt@nearapogee.com>
2013-08-15 17:33:20 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Launch::Application.config.secret_key_base = '82b5379b28c5e058228e25d20ac5a9fc48adc4f363655fc91361c54ddbdd0037e20dac40a8c437d26e02695d4ff5dfb21fef62dcf117bcf67ff0f649062f3af1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Launch::Application.config.session_store :cookie_store, key: '_launch_session'
